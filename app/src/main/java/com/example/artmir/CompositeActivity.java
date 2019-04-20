85096de1ccfc93a48b1d9192015c93d235be23a1
Your Name <Lenovo@DESKTOP-HOHBHT0.(none)>
2019-04-20 16:37:34 +0800

    4/20

---
+++ app/src/main/java/com/example/artmir/CompositeActivity.java
@@ -0,0 +1,12 @@
+package com.example.artmir;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CompositeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.composite);
+    }
+}
--- app/src/main/java/com/example/artmir/MainActivity.java
+++ app/src/main/java/com/example/artmir/MainActivity.java
@@ -1,30 +1,35 @@
 package com.example.artmir;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
+    private EditText edituser;
+    private  EditText editpass;
+    private TextView misspass;
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
-        loginButton = (Button) findViewById(R.id.login);
-        loginButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                        startActivity(intent);
-                    }
-                });
-
-
     }
+
+    public void loginonclick(View view){
+        Intent intent = new Intent(MainActivity.this, CompositeActivity.class);
+        startActivity(intent);
+    }
+
+    public void registeronclick(View view){
+        Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
